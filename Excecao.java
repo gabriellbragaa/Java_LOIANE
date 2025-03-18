dff5ce93d63cc7e091b2eec1834936e1a9a77e3b
gabriellbragaa <gabrielbragamartins.gb@gmail.com>
2025-03-18 17:21:10 -0300

    Tratamento de Excecoes

---
+++ Excecao.java
@@ -0,0 +1,41 @@
+/*******************  TRATAMENTO DE ERROS  ******************/ 
+
+// Thy e Catch 
+
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Excecao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {                                                                // try: vai executar o codigo
+            System.out.print("Digite um número inteiro: ");
+            
+            int numero = scanner.nextInt(); 
+            
+            int resultado = 10 / numero; 
+            
+
+            System.out.println("Resultado da divisão: " + resultado);
+
+        } catch (InputMismatchException e) {                                // catch: vai executar a excessao 
+            System.out.println("Erro: Você deve digitar um número inteiro válido.");
+            System.exit(0);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: Não é possível dividir por zero!");
+            System.exit(0);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro: Tentativa de acessar um índice inválido do array.");
+        } catch (Exception e) {
+            System.out.println("Erro inesperado: " + e.getMessage());
+        } finally {                                                         // finally: vai aparecer independente do resultado
+            System.out.println("Bloco finally: Finalizando o programa...");
+        }
+
+        System.out.println("Execução continua após o tratamento das exceções.");
+    }
+}
+
